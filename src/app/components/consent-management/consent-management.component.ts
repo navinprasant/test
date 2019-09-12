c8de361ba3639bfc3b713aeffac91dca7999b948
prashant-at-543154972686 <prashant@diro.io>
2019-09-12 19:54:07 +0530

    test

--- src/app/components/consent-management/consent-management.component.ts
+++ src/app/components/consent-management/consent-management.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonService } from '../../services/common.service';
+import { Router  } from '@angular/router';
 
 @Component({
   selector: 'app-consent-management',
@@ -7,13 +8,239 @@ import { CommonService } from '../../services/common.service';
   styleUrls: ['./consent-management.component.css']
 })
 export class ConsentManagementComponent implements OnInit {
+  defaultLang = window.navigator.language;
+  proVisible = true;
+  user = JSON.parse(localStorage.getItem("saveAllUserData"));
+  userName = this.user.doc.firstname;
+  userPro: any;
 
   constructor(
     private commonService: CommonService,
+    private router: Router
   ) { }
 
   ngOnInit() {
     this.commonService.stopLoader();
+      //     if($scope.defaultLang == "en" || $scope.defaultLang == "ru" || $scope.defaultLang == "fr" || $scope.defaultLang == "de" || $scope.defaultLang == "ar" || $scope.defaultLang == "zh-CN" || $scope.defaultLang == "nl" || $scope.defaultLang == "pt" || $scope.defaultLang == "es" || $scope.defaultLang == "vi" || $scope.defaultLang == "ja")
+      // {
+      //     $translate.use($scope.defaultLang);
+      // }
+      // else
+      // {
+      //     $translate.use('en');
+      // }
   }
+
+  showPro() {
+    this.proVisible = !this.proVisible;
+  }
+
+  signOut() {
+    localStorage.clear();
+    this.router.navigate(["/login"]);
+  }
+  gotomenu() {
+    this.router.navigate(['/mobilemenu']);
+  }
+
+  setUserData() {
+      if(this.user.data.doc.lastname != undefined && user.data.doc.lastname != "undefined" && user.data.doc.lastname != null && user.data.doc.lastname != "null" && user.data.doc.lastname != "")
+    {
+      this.userName = this.user.doc.firstname + " " + this.user.doc.lastname;
+    }
+    if(this.user.data.profileImage != undefined)
+    {
+      this.userPro = this.user.profileImage;
+    }
+
+    $scope.userMCC = user.data.doc.mcc;
+//     $scope.userMobile = user.data.doc.mobile;
+//     $scope.allOrgsrect = true;
+//     $scope.actionLoader = false;
+  }
+
+
+//     $scope.userMCC = user.data.doc.mcc;
+//     $scope.userMobile = user.data.doc.mobile;
+//     $scope.allOrgsrect = true;
+//     $scope.actionLoader = false;
+//     var token = localStorage.getItem("saveToken");
+//     document.body.style.opacity = "0.5";
+//     var url = getURL();
+//     $http({
+//       method: "POST",
+//       url: url.orginivitationdetails,
+//       data: {"mxid":user.data.mxid,"mobile":user.data.doc.mobile,"token":token},
+//       headers: {
+//         "Authorization": token
+//       }
+//     }).then(function(response){
+//       console.log(response);
+//       $scope.orgsList = response.data;
+//       $scope.allOrgsrect = false;
+//       $scope.actionLoader = true;
+//       document.body.style.opacity = "1";
+//     },function(err){
+//       console.log(err);
+//       $scope.allOrgsrect = true;
+//       $scope.actionLoader = false;
+//       document.body.style.opacity = "0.5";
+//       alert("Something went wrong");
+//     })
+//     $scope.getRequestTime = function(epoc)
+//     {
+//       console.log(epoc);
+//       var time = getPrettyTiming(epoc);
+//       return time;
+//     }
+//     $scope.acceptOrgReq = function(orgData,index)
+//     {
+//       $scope.actionLoader = false;
+//       document.body.style.opacity = "0.5";
+//       var user = JSON.parse(localStorage.getItem("saveAllUserData"));
+//       var token = localStorage.getItem("saveToken");
+//       let urlToken = guid();
+//       var json = {"mxid":user.data.mxid,"status":true,"apikey":orgData.apikey,"orgid":orgData.orgid};
+//       var url = getURL();
+//       $http({
+//         method: "POST",
+//         url: url.orgverifiedinvite,
+//         data: json,
+//         headers: {
+//           "Authorization": token
+//         }
+//       }).then(function(response){
+//         console.log(response);
+//         if(response.data.message == "successful")
+//         {
+//           $scope.orgsList.pendingrequest.splice(index,1);
+//           var json = orgData;
+//           json.status = true;
+//           json.eptime = new Date().getTime();
+//           if($scope.orgsList.allorgdetail != undefined)
+//           {
+//             $scope.orgsList.allorgdetail.unshift(json);
+//           }
+//           else
+//           {
+//             $scope.orgsList.allorgdetail = [];
+//             $scope.orgsList.allorgdetail.push(json);
+//           }
+//           $scope.actionLoader = true;
+//           document.body.style.opacity = "1";	
+//           window.location.href = orgData.boardurl;
+//         }
+//         else
+//         {
+//           $scope.actionLoader = true;
+//           document.body.style.opacity = "1";
+//           alert("Something went wrong");
+//         }
+//       },function(err){
+//         console.log(err);
+//         $scope.actionLoader = true;
+//         document.body.style.opacity = "1";
+//         alert("Something went wrong");
+//       })
+//     }
+//     $scope.rejectOrgReq = function(orgData,index)
+//     {
+//       $scope.actionLoader = false;
+//       document.body.style.opacity = "0.5";
+//       var user = JSON.parse(localStorage.getItem("saveAllUserData"));
+//       var token = localStorage.getItem("saveToken");
+//       var json = {"mxid":user.data.mxid,"status":false,"apikey":orgData.apikey,"orgid":orgData.orgid,"token":token};
+//       var url = getURL();
+//       $http({
+//         method: "POST",
+//         url: url.orgverifiedinvite,
+//         data: json,
+//         headers: {
+//           "Authorization": token
+//         }
+//       }).then(function(response){
+//         console.log(response);
+//         if(response.data.message == "successful")
+//         {
+//           $scope.orgsList.pendingrequest.splice(index,1);
+//           var json = orgData;
+//           json.status = false;
+//           json.eptime = new Date().getTime();
+//           if($scope.orgsList.allorgdetail != undefined)
+//           {
+//             $scope.orgsList.allorgdetail.unshift(json);
+//           }
+//           else
+//           {
+//             $scope.orgsList.allorgdetail = [];
+//             $scope.orgsList.allorgdetail.push(json);
+//           }
+//           $scope.actionLoader = true;
+//           document.body.style.opacity = "1";
+//         }
+//         else
+//         {
+//           $scope.actionLoader = true;
+//           document.body.style.opacity = "1";
+//           alert("Something went wrong");
+//         }
+//       },function(err){
+//         console.log(err);
+//         $scope.actionLoader = true;
+//         document.body.style.opacity = "1";
+//         alert("Something went wrong");
+//       })
+//     }
+//     $scope.changeSwitchStatus = function(orgData,status,index)
+//     {
+//       $scope.actionLoader = false;
+//       document.body.style.opacity = "0.5";
+//       var user = JSON.parse(localStorage.getItem("saveAllUserData"));
+//       var token = localStorage.getItem("saveToken");
+//       var url = getURL();
+//       $http({
+//         method: "POST",
+//         url: url.orgswitchstatus,
+//         data: {"mxid":user.data.mxid,"orgid":orgData.orgid,"status":status,"token":token},
+//         headers: {
+//           "Authorization": token
+//         }
+//       }).then(function(response){
+//         console.log(response);
+//         if(response.data.message == "successful")
+//         {
+//           $scope.actionLoader = true;
+//           document.body.style.opacity = "1";
+//           if(status == true && orgData.boardurl)
+//           {
+//             window.location.href = orgData.boardurl;
+//           }
+//         }
+//         else
+//         {
+//           if(status == true)
+//           {
+//             $scope.orgsList.allorgdetail[index].status = false;
+//             $scope.actionLoader = true;
+//             document.body.style.opacity = "1";
+//             alert("Something went wrong");
+//           }
+//           else
+//           if(status == false)
+//           {
+//             $scope.orgsList.allorgdetail[index].status = true;
+//             $scope.actionLoader = true;
+//             document.body.style.opacity = "1";
+//             alert("Something went wrong");
+//           }
+//         }
+//       },function(err){
+//         console.log(err);
+//         $scope.actionLoader = true;
+//         document.body.style.opacity = "1";
+//         alert("Something went wrong");
+//       })
+//     }
+
 
 }
--- src/app/components/mobileprofile/mobileprofile.component.ts
+++ src/app/components/mobileprofile/mobileprofile.component.ts
@@ -12,4 +12,139 @@ export class MobileprofileComponent implements OnInit {
   ngOnInit() {
   }
 
+  // app.controller('mobilemenuController', ['$scope', function($scope) {
+
+  //   console.log(window.location.port);
+  //   $scope.closeMenu = function()
+  //   {
+    
+  //      window.history.back();
+  //   }
+  //   $scope.loadAppStore = function()
+  //   {
+  //       window.open("https://itunes.apple.com/in/app/diro-identity/id1408537104?mt=8");
+  //   }
+  //   $scope.loadPlayStore = function()
+  //   {
+  //       window.open("https://play.google.com/store/apps/details?id=com.diro.kyc");
+  //   }
+  //   }])
+
+
+
+
+
+
+
+
+
+  // app.controller('mobileprofileController',['$rootScope','$scope','$window','$http','$location',function ($rootScope,$scope,$window,$http,$location) {
+  //   $scope.gotomenu = function()
+  //   {
+  //      $location.path('/mobilemenu');   
+  //   }
+  //   $scope.goEditProfile = function()
+  //   {
+  //     $location.path('/profile'); 
+  //   }
+  //   function getData(files){
+  //     var addArr = [];
+  //     var idArr = [];
+  //     var bankArr = [];
+  //     var other = [];
+  //     for(var i = 0;i<files.length;i++)
+  //     {
+  //       if(files[i].category == "idProof" || files[i].category == "idproof")
+  //       {
+  //         idArr.push(files[i]);
+  //       }
+  //       else if(files[i].category == "addressProof")
+  //       {
+  //         addArr.push(files[i]);
+  //       }
+  //       else if(files[i].category == "bankstatement")
+  //       {
+  //         bankArr.push(files[i]);
+  //       }
+  //       else if(files[i].category == "other")
+  //       {
+  //         other.push(files[i]);
+  //       }
+  //     }
+  //     if(addArr.length > 0)
+  //     {
+  //       formatData(addArr);
+  //     }
+  //     if(idArr.length > 0)
+  //     {
+  //       formatData(idArr);
+  //     }
+  //     if(bankArr.length > 0)
+  //     {
+  //       formatData(bankArr);
+  //     }
+  //     if(other.length > 0)
+  //     {
+  //       formatData(other);
+  //     }
+  //   }
+  //   function formatData(arr)
+  //   {
+  //     var flag = 0;
+  //     for(var i = 0;i<arr.length;i++)
+  //     {
+  //       if(arr[i].status == 'verified')
+  //       {
+  //         $scope.kycFiles.push(arr[i]);
+  //         flag = 1;
+  //         break;
+  //       }
+  //     }
+  //     if(flag == 0)
+  //     {
+  //       var maxEptime = arr[0].eptime;
+  //       var maxArrVal = arr[0];
+  //       for(var i = 1;i<arr.length;i++)
+  //       {
+  //         if(maxEptime < arr[i].eptime)
+  //         {
+  //           maxEptime = arr[i].eptime;
+  //           maxArrVal = arr[i];
+  //         }
+  //       }
+  //       $scope.kycFiles.push(maxArrVal);
+  //     }
+  //     console.log($scope.kycFiles);
+  //   }
+  //   $scope.getFilename = function(str)
+  //   {
+  //     if(str.length > 15)
+  //     {
+  //       return str.substring(0,15) + "...";
+  //     }
+  //     else
+  //     {
+  //       return str;
+  //     }
+  //   }
+  //   function init()
+  //   {
+  //     $scope.user = JSON.parse(localStorage.getItem("saveAllUserData"));
+  //     $scope.memberSince = getTimeFormat($scope.user.data.doc.crtime);
+  //     $scope.kycFiles = [];
+  //     getData($scope.user.data.kycDoc.files);
+  //     console.log($scope.user);
+  //   }
+  //   init();
+  // }])
+
+
+
+
+
+
+
+
+
+
 }
